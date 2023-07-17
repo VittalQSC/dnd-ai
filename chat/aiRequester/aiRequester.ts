5f2eb5a4e8116031169f3c9420f0bde18577be96
Vitali Shatsou <vitshacov@gmail.com>
2023-07-17 11:15:32 +0200

    rephrase intent prompt

--- chat/aiRequester/aiRequester.ts
+++ chat/aiRequester/aiRequester.ts
@@ -11,6 +11,12 @@ async function createMemory() {
   return memory;
 }
 
+async function delay(ms: number) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
 export const aiRequester: IRequester = {
   async init() {
     this.memory = await createMemory();
@@ -18,7 +24,11 @@ export const aiRequester: IRequester = {
     this.chain = new LLMChain({
       llm: this.model,
       prompt: PromptTemplate.fromTemplate(`
-The following is a D&D game between a human and an AI, where human is a game master.
+The following is a adventure game between a human and an AI, where human is a game master.
+Your goal is to finish the quest and get the reward.
+You should win as soon as possible.
+You are the warrior, you like to fight and you are not very smart.
+Also you like open chests and get loot from them.
 
 Current conversation:
 {chat_history}
@@ -30,9 +40,11 @@ AI:`),
 
   async request(question: string) {
     question =
-      question || "Let's start. Give me the theme of the quest, please!";
+      question ||
+      "Let's start. Give me the title of the quest in a couple of words, please!";
     // console.log("@aiRequester.request question", { question });
     const result = await this.chain.call({ input: question });
+    // await delay(2000);
     console.log("\n\n@aiRequester.request", result.text, "\n\n");
     return result.text;
   },
--- game/GameMaster.ts
+++ game/GameMaster.ts
@@ -7,11 +7,13 @@ import {
 } from "langchain/agents";
 import { BufferMemory } from "langchain/memory";
 import { RollDiceTool } from "./tools/RollDice.Tool";
-import { ChainTool, Tool } from "langchain/tools";
+import { ChainTool, DynamicTool, Tool } from "langchain/tools";
 import { VectorDBQAChain } from "langchain/chains";
 import { MemoryVectorStore } from "langchain/vectorstores/memory";
 import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 import { ChestLootTool } from "./tools/ChestLoot.Tool";
+
+let currentArea = "tavern in the village";
 
 export class GameMaster {
   memory: BufferMemory = new BufferMemory();
@@ -28,9 +30,37 @@ export class GameMaster {
     this.tools = [
       new RollDiceTool(),
       new ChestLootTool(),
+      new DynamicTool({
+        name: "set-current-area",
+        description: `Use it when player entering another area. INPUT: short area description"`,
+        async func(input) {
+          currentArea = input;
+          console.log("@set-current-area", currentArea);
+          return input;
+        }
+      }),
+      new DynamicTool({
+        name: "get-current-area",
+        description: "Use it when you need to know player's current area.",
+        async func() {
+          console.log("@get-current-area", currentArea);
+          return currentArea;
+        }
+      }),
       new ChainTool({
-        name: "map",
-        description: `Use it to navigate the player through the game.`,
+        name: "maps",
+        description: `Use it to navigate the player through the game.
+        
+        connections between areas are described by "->" symbol.
+
+        location map describes the specific location.
+        location map consists of areas connected to each other.
+
+        each area consists of a list of instances, separated by ";" symbol.
+        some instances are dispatching the situation when player entering the area.
+        some instances are just objects that player can interact with.
+        area format is the following: <object1>;<object2>;<object3>
+        `,
         chain: await this.#createMapChain(),
       }),
     ];
@@ -55,7 +85,7 @@ export class GameMaster {
   }
 
   async #createMapChain() {
-    const loader = new DirectoryLoader("map", {
+    const loader = new DirectoryLoader("map2", {
       ".txt": (path) => new TextLoader(path),
     });
     const docs = await loader.load();
@@ -68,41 +98,15 @@ export class GameMaster {
 }
 
 const PROMPT = `
-You are a dungeon master in D&D game.
-Tell player the story and plot of the game.
-Tell player quest description that player should complete.
+You are a dungeon master in adventure game.
+Tell player the story and plot of the game at once.
+Tell player his goal to complete the adventure at once. The goal should be simple and clear.
 You are talking to the player, player making some actions and you are reacting to them.
-Some player actions should be checked by rolling a dice. Roll the dice for the player and tell him the result of rolling.
+Some player actions should be checked by rolling a dice. Roll the dice for the player and tell him the result of rolling and what dice was rolled.
+Ask player what he wants to do next.
 
-You are given a set of maps. Use them to create the quest and to navigate the player.
-Each map has title, sequence and legend.
-Global map is a sequence of locations connected by "->" next to each other.
-Location map is a sequence of spots connected by "->" next to each other.
-To pass the location player should go through all spots sequence one by one in the location.
+Create the locations of the game strongly according to the map. No additional locations or areas should be created.
+Player starting in tavern in the village.
 
-Player is always located in one of the spots. He can move between spots in the location.
-If Player is in the first spot of the location, he can return back to the previous location.
-If Player is in the last spot of the location, he can go to the next location.
-Tavern spot is the starting point of the player
-Player should go between spots from left to the right to go further to desired destination.
-Player can not go to the spot that is not connected to the current spot. But Player can go to desired spot and something will happen on the next spot.
-Player don't see the next spot until he will reach it.
-Player should not know the map, he should explore it by himself. You should not tell the Player what waits for him in the next spot.
-When Player entering new spot, describe in details the spot.
-Track player's spot where he is currently located.
-Angry wizards is impossible to pass without solving their riddle.
+All riddles should be unique. You can use the same riddle only once.
 `;
-
-// Player should go through all locations in order to finish the quest.
-// Player should solve simple task in each location to go to the next location. Create the task for each location and describe it to the player.
-// Player can move to the spot if there is a connection between the spots.
-// Player can move from one location to another if he reached first spot of the current location (then can can return to the previous location) or the last spot of the current location (then he can go to the next location).
-
-// 0cmwk
-
-// 0 - starting location
-// c - location with a chest
-// m - location with a monster
-// k - location with a key
-// w - location with a wizard, he will let you pass if you will solve his riddle
-// o - empty location, player can not move there
